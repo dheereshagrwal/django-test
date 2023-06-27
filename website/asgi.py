76a82ba2d46eb4c94702cd25b0e7b3cc135a056b
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-06-27 18:05:43 +0530

    name changed

--- website/asgi.py
+++ website/asgi.py
@@ -1,5 +1,4 @@
 """
-ASGI config for hello_world_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_world_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
 application = get_asgi_application()
--- website/wsgi.py
+++ website/wsgi.py
@@ -1,5 +1,4 @@
 """
-WSGI config for hello_world_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +10,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_world_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
 application = get_wsgi_application()
 app = application
